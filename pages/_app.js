28b5ab2f7f223a63cbc750dad2c8f8463781268a
Michael Young <msyoung2012@gmail.com>
2022-08-06 12:53:26 -0400

    Linting
    I guess

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
-import '../styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react'
-
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-  return(
+  return (
     <ChakraProvider>
-<Component {...pageProps} />
- </ChakraProvider>
- )
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import ResponsiveEmbed from 'react-responsive-embed'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import ResponsiveEmbed from "react-responsive-embed";
 import {
   Flex,
   Container,
@@ -11,129 +11,161 @@ import {
   Button,
   Icon,
   IconProps,
-  Box, Divider, HStack
-} from '@chakra-ui/react';
-import Script from 'next/script'
-
-
+  Box,
+  Divider,
+  HStack,
+} from "@chakra-ui/react";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Monkeypox Tracker - Monkeypox Statistics</title>
-        <meta name="description" content="Statistics and information on the 2022 Monkeypox outbreak, including maps, charts, and tables from sources around the world." />
+        <meta
+          name="description"
+          content="Statistics and information on the 2022 Monkeypox outbreak, including maps, charts, and tables from sources around the world."
+        />
 
-          <meta property="og:title" content="Monkeypox Tracker - Monkeypox Statistics" />
-          <meta property="og:description" content="Statistics and information on the 2022 Monkeypox outbreak, including maps, charts, and tables from sources around the world." />
+        <meta
+          property="og:title"
+          content="Monkeypox Tracker - Monkeypox Statistics"
+        />
+        <meta
+          property="og:description"
+          content="Statistics and information on the 2022 Monkeypox outbreak, including maps, charts, and tables from sources around the world."
+        />
 
-          <meta property="og:url" content="https://monkeypoxtracker.net/" />
-          <meta property="og:image" content="https://monkeypoxtracker.net/socialImg.png" />
-          <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://monkeypoxtracker.net/" />
+        <meta
+          property="og:image"
+          content="https://monkeypoxtracker.net/socialImg.png"
+        />
+        <meta property="og:type" content="website" />
 
-          <meta name="twitter:card" content="summary_large_image"/>
-          <meta property="twitter:description" content="Statistics and information on the 2022 Monkeypox outbreak, including maps, charts, and tables from sources around the world." />
-          <meta property="twitter:image" content="https://monkeypoxtracker.net/socialImg.png" />
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4489327921275613"
-     crossOrigin="anonymous" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:description"
+          content="Statistics and information on the 2022 Monkeypox outbreak, including maps, charts, and tables from sources around the world."
+        />
+        <meta
+          property="twitter:image"
+          content="https://monkeypoxtracker.net/socialImg.png"
+        />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4489327921275613"
+          crossOrigin="anonymous"
+        />
 
-     <script async src="https://www.googletagmanager.com/gtag/js?id=G-DFXC4Y1G0E"></script>
-     <script  dangerouslySetInnerHTML={{ __html:`
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-DFXC4Y1G0E"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
        window.dataLayer = window.dataLayer || [];
        function gtag(){dataLayer.push(arguments);}
        gtag('js', new Date());
 
-       gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`,}}
-     />
-
+       gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`,
+          }}
+        />
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-      <Box mt={6}>
-      <Text fontSize='2xl' as='b' mt={8}>MonkeypoxTracker.net</Text>
-      <Stack flex={{ base: 1, md: 0 }}
-          justify={'flex-end'}
-          direction={'row'}
-          spacing={6}
-          mt={-9}>
+        <Box mt={6}>
+          <Text fontSize="2xl" as="b" mt={8}>
+            MonkeypoxTracker.net
+          </Text>
+          <Stack
+            flex={{ base: 1, md: 0 }}
+            justify={"flex-end"}
+            direction={"row"}
+            spacing={6}
+            mt={-9}
+          ></Stack>
+        </Box>
 
-      </Stack>
-      </Box>
+        <Container maxW={"5xl"}>
+          <Stack
+            textAlign={"center"}
+            align={"center"}
+            spacing={{ base: 8, md: 10 }}
+            py={{ base: 20, md: 28 }}
+          >
+            <Heading
+              fontWeight={600}
+              fontSize={{ base: "3xl", sm: "4xl", md: "6xl" }}
+              lineHeight={"110%"}
+            >
+              There are{" "}
+              <Text as={"span"} color={"orange.400"}>
+                375
+              </Text>{" "}
+              active cases.
+            </Heading>
+            <Text color={"gray.500"} maxW={"3xl"}>
+              The first human case of monkeypox was recorded in 1970 in the
+              Democratic Republic of Congo. Since then, monkeypox has been
+              reported in humans in other central and western African countries.
+              In 2022, an unusual amount of monkeypox cases around the world
+              have prompted concern and interest from health officials and the
+              public.
+              <br />
+              <br />
+              This site is dedicated to tracking the spread of the disease
+              during this outbreak, and is updated every few hours.
+            </Text>
+          </Stack>
+        </Container>
 
+        <Container maxW={"5xl"}>
+          <Box mb={12} maxH={500}>
+            <ResponsiveEmbed
+              ratio="10:9"
+              title="Monkeypox cases by country"
+              aria-label="Map"
+              id="datawrapper-chart-req8A"
+              src="https://datawrapper.dwcdn.net/req8A/2/"
+              scrolling="no"
+            />
+          </Box>
 
-      <Container maxW={'5xl'}>
-           <Stack
-             textAlign={'center'}
-             align={'center'}
-             spacing={{ base: 8, md: 10 }}
-             py={{ base: 20, md: 28 }}>
-             <Heading
-               fontWeight={600}
-               fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
-               lineHeight={'110%'}>
-               There are {' '}
-               <Text as={'span'} color={'orange.400'}>
-                 375
-               </Text>
-               {' '} active cases.
-             </Heading>
-             <Text color={'gray.500'} maxW={'3xl'}>
-               The first human case of monkeypox was recorded in 1970 in the Democratic Republic of Congo.
-               Since then, monkeypox has been reported in humans in other central and western African countries. In 2022, an unusual
-               amount of monkeypox cases around the world have prompted concern and interest from health officials and the public.
-               <br/><br/>
-               This site is dedicated to tracking the spread of the disease during this outbreak, and is updated every few hours.
-             </Text>
-           </Stack>
-       </Container>
+          <Box mb={12}>
+            <ResponsiveEmbed
+              title="Monkeypox cases over time"
+              ratio="10:9"
+              aria-label="Interactive line chart"
+              id="datawrapper-chart-8BTii"
+              src="https://datawrapper.dwcdn.net/8BTii/2/"
+              scrolling="no"
+            />
+          </Box>
 
-      <Container   maxW={'5xl'}>
-      <Box mb={12} maxH={500}>
-    <ResponsiveEmbed ratio="10:9" title="Monkeypox cases by country"
-    aria-label="Map"
-    id="datawrapper-chart-req8A"
-    src="https://datawrapper.dwcdn.net/req8A/2/"
-    scrolling="no"
-
-    />
-    </Box>
-
-    <Box mb={12} >
-        <ResponsiveEmbed title="Monkeypox cases over time" ratio="10:9"
-    aria-label="Interactive line chart"
-    id="datawrapper-chart-8BTii"
-    src="https://datawrapper.dwcdn.net/8BTii/2/"
-    scrolling="no"
-      />
-    </Box>
-
-    <Box mb={12} maxH={500}>
-    <ResponsiveEmbed ratio="1:1"
-    title="Monkeypox cases by country"
-    aria-label="Table"
-    id="datawrapper-chart-gvUWM"
-    src="https://datawrapper.dwcdn.net/gvUWM/3/"
-    scrolling="yes"
-    />
-
-    </Box>
-
-
-
-
-</Container>
-<Divider />
-<Container>
-
-<Text mt={12} mb={12}>Want to raise awareness? You can share this site on social media. <br/> <br/>Check back for daily updates.</Text>
-
-</Container>
-
+          <Box mb={12} maxH={500}>
+            <ResponsiveEmbed
+              ratio="1:1"
+              title="Monkeypox cases by country"
+              aria-label="Table"
+              id="datawrapper-chart-gvUWM"
+              src="https://datawrapper.dwcdn.net/gvUWM/3/"
+              scrolling="yes"
+            />
+          </Box>
+        </Container>
+        <Divider />
+        <Container>
+          <Text mt={12} mb={12}>
+            Want to raise awareness? You can share this site on social media.{" "}
+            <br /> <br />
+            Check back for daily updates.
+          </Text>
+        </Container>
       </main>
-
-
     </div>
-  )
+  );
 }

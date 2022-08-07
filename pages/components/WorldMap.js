fc206ac70a40c4209478e07c05239cc01909f457
Michael Young <msyoung2012@gmail.com>
2022-08-06 21:14:00 -0400

    Mocking worldmap case data
    Have 2 different json objs that need to be sync'd

--- pages/components/WorldMap.js
+++ pages/components/WorldMap.js
@@ -1,5 +1,4 @@
 import React, { memo, useEffect, useState } from "react";
-
 import {
   ZoomableGroup,
   ComposableMap,
@@ -7,26 +6,30 @@ import {
   Geography,
 } from "react-simple-maps";
 import { scaleLinear } from "d3-scale";
-import { usePapaParse } from "react-papaparse";
-
-import {
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-  TableContainer,
-} from "@chakra-ui/react";
-
-import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons";
-import { useTable, useSortBy } from "react-table";
 
 const colorScale = scaleLinear().domain([0, 500]).range(["#ebf7f9", "#189ed3"]);
 
 const MapChart = ({ setTooltipContent }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const url =
+      "https://www.cdc.gov/poxvirus/monkeypox/modules/data-viz/mpx_confirmed_cases_data_bites_prod.json?v=2022-01-01T23%3A00%3A00.000Z ";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setData(json.data);
+        console.log(json.data);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div data-tip="">
       <ComposableMap projection="geoMercator">
@@ -39,7 +42,7 @@ const MapChart = ({ setTooltipContent }) => {
                 stroke="#ffffff"
                 strokeWidth={1}
                 onMouseEnter={() => {
-                  setTooltipContent(`${geo.properties.name} `);
+                  setTooltipContent(`${geo.properties.name} ${data[0].Cases}`);
                 }}
                 onMouseLeave={() => {
                   setTooltipContent("");

f8a1a6c7ae63aa6ca6fcd812b5cb7de33037de6a
Michael Young <msyoung2012@gmail.com>
2022-08-21 17:53:13 -0400

    Create responsive navbar behavior
    Added hamburger menu, responsive navbar, and styles

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -2,41 +2,108 @@ import Link from "next/link";
 import {
   Container,
   Heading,
-  Stack,
+  HStack,
   Text,
   Box,
   Divider,
   Tooltip,
   Center,
+  Flex,
+  IconButton,
+  Spacer,
 } from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [display, changeDisplay] = useState("none");
+
   return (
     <nav>
-      <Box mt={6} mr={5} ml={5} style={{ marginLeft: "1rem" }}>
-        <Text fontSize="2xl" as="b" mt={8} ml={5}>
-          <Link href="/">
-            <a>MonkeypoxTracker</a>
-          </Link>
-        </Text>
-        <Stack
-          flex={{ base: 1, md: 0 }}
-          justify={"flex-end"}
-          direction={"row"}
-          spacing={6}
-          mt={-9}
-          mr={5}
+      <Flex
+        p={5}
+        borderBottom={1}
+        borderStyle={"solid"}
+        borderColor={"gray.200"}
+      >
+        <Flex top="1rem" right="1rem" align="center">
+          {/*LOGO*/}
+          <Heading fontSize="2xl">
+            <Link href="/" aria-label="Home" my={5} w="100%">
+              <a>MonkeypoxTracker</a>
+            </Link>
+          </Heading>
+        </Flex>
+        <Spacer />
+        <Flex top="1rem" right="1rem" align="center">
+          {/* DESKTOP */}
+
+          <Flex display={["none", "none", "flex", "flex"]}>
+            <HStack spacing={10}>
+              <Link href="/countries" aria-label="Countries" m={5} w="100%">
+                <a>Country data</a>
+              </Link>
+              <Link href="/states" aria-label="States" m={5} w="100%">
+                <a>State data</a>
+              </Link>
+            </HStack>
+          </Flex>
+
+          {/* MOBILE */}
+          <IconButton
+            aria-label="Open Menu"
+            size="lg"
+            mr={2}
+            icon={<HamburgerIcon />}
+            onClick={() => changeDisplay("flex")}
+            display={["flex", "flex", "none", "none"]}
+          ></IconButton>
+        </Flex>
+
+        {/* MOBILE CONTENT */}
+
+        <Flex
+          w="100vw"
+          display={display}
+          bgColor="gray.50"
+          zIndex={20}
+          h="100vh"
+          pos="fixed"
+          top="0"
+          left="0"
+          overflowY="auto"
+          flexDir="column"
         >
-          <Center>
-            <Link href="/countries">
-              <a>By country</a>
+          <HStack m={5}>
+            <Flex top="1rem" align="center">
+              {/*LOGO*/}
+              <Heading fontSize="2xl">
+                <Link href="/" aria-label="Home" my={5} w="100%">
+                  <a onClick={() => changeDisplay("none")}>MonkeypoxTracker</a>
+                </Link>
+              </Heading>
+            </Flex>
+            <Spacer />
+            <Flex>
+              <IconButton
+                mr={5}
+                aria-label="Close Menu"
+                size="lg"
+                icon={<CloseIcon />}
+                onClick={() => changeDisplay("none")}
+              ></IconButton>
+            </Flex>
+          </HStack>
+          <Flex flexDir="column" align="center">
+            <Link href="/countries" aria-label="Countries" my={5} w="100%">
+              <a onClick={() => changeDisplay("none")}>Country data</a>
             </Link>
-            <Link pl={5} href="/states">
-              <a>By state</a>
+            <Link pl={5} href="/states" aria-label="States" my={5} w="100%">
+              <a onClick={() => changeDisplay("none")}>State data</a>
             </Link>
-          </Center>
-        </Stack>
-      </Box>
+          </Flex>
+        </Flex>
+      </Flex>
     </nav>
   );
 };

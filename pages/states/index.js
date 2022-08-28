598e8f6d23bb9f0007eedd072b6d7c4b1ab2a591
Michael Young <msyoung2012@gmail.com>
2022-08-28 17:26:35 -0400

    expanding hotfix to additional pages

--- pages/states/index.js
+++ pages/states/index.js
@@ -48,6 +48,24 @@ const States = ({ stateVals }) => {
   return (
     <>
       <Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+
         <title>Monkeypox Tracker | States</title>
         <meta
           name="description"
--- pages/us/testing.js
+++ pages/us/testing.js
@@ -15,6 +15,24 @@ const About = () => {
   return (
     <>
       <Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+
         <title>U.S. Testing Information | Monkeypox Tracker</title>
         <meta
           name="description"

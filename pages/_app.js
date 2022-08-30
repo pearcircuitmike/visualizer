f25b61cdc1699883fb70a94ec59a738d0339a7e3
Michael Young <msyoung2012@gmail.com>
2022-08-30 09:34:04 -0400

    Hotfix GA attempt 2?
    per https://stackoverflow.com/questions/60411351/how-to-use-google-analytics-with-next-js-app

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,31 @@
 import "../styles/globals.css";
 import Layout from "./components/Layout";
+import Script from "next/script";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+        `}
+      </Script>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
   );
 }
 
--- pages/states/[state].js
+++ pages/states/[state].js
@@ -145,24 +145,6 @@ const StateDetails = ({ state }) => {
   return (
     <div key={router.pathname}>
       <Head>
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
-
         <title>{stateName} | Monkeypox Tracker</title>
         <meta
           name="description"

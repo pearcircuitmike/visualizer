82015b677c774cb6c938e02ccf89a0c7598e424a
Michael Young <msyoung2012@gmail.com>
2022-08-30 09:37:57 -0400

    attempt 3
    wrap closer to components

--- pages/_app.js
+++ pages/_app.js
@@ -7,21 +7,22 @@ import { ChakraProvider } from "@chakra-ui/react";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
+      <ChakraProvider>
+        <Layout>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
         `}
-      </Script>
-      <ChakraProvider>
-        <Layout>
+          </Script>
+
           <Component {...pageProps} />
         </Layout>
       </ChakraProvider>
